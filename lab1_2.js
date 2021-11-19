f58b891681ddfbb1ab15bf2f8b637d7e0190b091
CatboyWizard <90338417+CatboyWizard@users.noreply.github.com>
2021-11-19 13:16:59 -0500

    Add files via upload

---
+++ lab1_2.js
@@ -0,0 +1,24 @@
+// Javascript Document
+var numberOfApples = 10;
+document.write("the number of apples I have is" + numberOfApples + "<br>")
+numberOfApples++;
+document.write("the number of apples I have is" + numberOfApples + "<br>")
+numberOfApples++;
+document.write("the number of apples I have is" + numberOfApples + "<br>")
+numberOfApples++;
+document.write("the number of apples I have is" + numberOfApples + "<br>")
+numberOfApples++;
+document.write("the number of apples I have is" + numberOfApples + "<br>")
+numberOfApples++;
+document.write("the number of apples I have is" + numberOfApples + "<br>")
+numberOfApples++;
+
+document.write("<br><br>")
+
+var numberOfNinja = 100;
+document.write("There are "+ --numberOfNinja + " ninjas in the room. <br>")
+document.write("There are "+ --numberOfNinja + " ninjas in the room. <br>")
+document.write("There are "+ --numberOfNinja + " ninjas in the room. <br>")
+document.write("There are "+ --numberOfNinja + " ninjas in the room. <br>")
+document.write("There are "+ --numberOfNinja + " ninjas in the room. <br>")
+document.write("Created by Kevin McHugh")
